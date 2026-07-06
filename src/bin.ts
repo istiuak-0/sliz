52f7a816c222be3e2b7e2558d629b468bb4d732f
istiuak <istiuak.0@gmail.com>
2026-07-06 20:51:10 +0600

    setup project

---
+++ src/bin.ts
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+const [, , command] = process.argv
+
+switch (command) {
+	case 'compile':
+		console.log('compile');
+
+		break
+
+	default:
+		break
+}
---
+++ tsup.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'tsup'
+
+export default defineConfig({
+   entry: ['src/bin.ts'],
+   format: ['esm'],
+   dts: { resolve: true },
+   sourcemap: false,
+   clean: true,
+   splitting: true,
+   minify: false,
+   target: 'es2022',
+   outDir: 'dist',
+})
